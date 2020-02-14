e6d3eb2fafbbe28c08e49b6b30a9fbf200a7b3b9
Max McKenzie <you@example.com>
2020-02-14 11:55:40 +0100

    added new name from current dev project

--- generator/template/nightwatch.conf.js
+++ generator/template/nightwatch.conf.js
@@ -1,3 +1,3 @@
-const config = require('vue-cli-plugin-e2e-nightwatch-cucumber/nightwatch.conf');
+const config = require('vue-cli-plugin-e2e-nightwatch-cucumber-repack/nightwatch.conf');
 
 module.exports = Object.assign({}, config);
